2e0ee04eaa21c309eda63322c71a9beda8631281
Joaofelixz <joao.nascimento9@estudante.ifms.edu.br>
2024-11-25 14:35:32 -0400

    25/11

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 export default function Registrar() {
+ const [nome, setNome] = useState('');
+ const [email, setEmail] = useState('');
 
+  const registrar = (event) =>{
+event.preverDefault();
+ await fetch ("htpps")
+  }
 
   return (
-        
+        <main>
+          <form action="">
+            <input type="text" value={nome} onChange={(event)=> setNome(event.target.value)}/>
+            <input type="email" value={email} onChange={(event)=> setEmail(event.target.value)}/>
+            <button>Salvar</button>
+          </form>
+        </main>
   );
 }
