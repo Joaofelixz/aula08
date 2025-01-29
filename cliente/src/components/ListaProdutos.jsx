08e80e1c3f8d60dc5281a7532585a5b574958626
Joaofelixz <159342228+Joaofelixz@users.noreply.github.com>
2025-01-29 00:15:53 -0400

    Atualizar o ListaProdutos.jsx

--- cliente/src/components/ListaProdutos.jsx
+++ cliente/src/components/ListaProdutos.jsx
@@ -1,19 +1,30 @@
-/* eslint-disable react/prop-types */
-import styles from '../styles/listaProdutos.module.css'
+import listaProdutosStyles from '../styles/ListaProdutos.module.css';
 
-export default function ListaProdutos({ produtos }) {
+export default function ListaProdutos({ produtos, removerCarro }) {
   return (
-    <>
-      <ul className={styles.bloco}>
-        {produtos.map(produto => (
-          <li key={produto.id}>
-            <h2>{produto.title}</h2>
-            <p>{produto.description}</p>
-            <p>Preço: {produto.price}</p>
-            <img src={produto.image} alt={produto.title} width={100} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className={listaProdutosStyles.displayProdutos}>
+      {produtos.map(produto => (
+        <div key={produto.id} className={listaProdutosStyles.produtoItem}>
+          <h2 className={listaProdutosStyles.produtoNome}>{produto.nomeCarro}</h2>
+          <img className={listaProdutosStyles.produtoImagem} src={produto.imagem} alt={produto.imagem} />
+          <p className={listaProdutosStyles.produtoPreco}>Preço: R${produto.preco}</p>
+          <p className={listaProdutosStyles.produtoDetalhes}>
+          <span>Cor:</span> {produto.cor}
+          </p>
+          <div className={listaProdutosStyles.botaoContainer}>
+            <a href={`/alterar/${produto.id}`}>
+    <button className={listaProdutosStyles.botaoEditar}>Editar</button>
+            </a>
+            <button
+  className={listaProdutosStyles.botaoRemover}
+  onClick={() => removerCarro(produto.id)}
+>
+  Remover
+</button>
+
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
