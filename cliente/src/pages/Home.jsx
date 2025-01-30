09a947f9343a5b8d9c38e1409d95b6cf220301aa
Joaofelixz <joao.nascimento9@estudante.ifms.edu.br>
2025-01-29 20:10:14 -0400

    29/01

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,64 +1,68 @@
 import { useEffect, useState } from "react";
-import {jsPDF} from "jspdf";
-import "jspdf-autotable";
-import {button} from '@mui/material';
+import ListaProdutos from '../components/ListaProdutos';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import homeStyles from '../styles/Home.module.css';
 
 export default function Home() {
+    const [ListaCarros, setListaCarros] = useState([]);
+    useEffect(() => {
+        const buscarCarro = async () => {
+          try {
+            const resposta = await fetch("http://localhost:3000/carros");
+            const dados = await resposta.json();
+            setListaCarros(dados);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        buscarCarro();
+      }, []);
 
-  const [usuarios, setUsuarios] = useState([]);
-
-  useEffect(() => {
-    const buscarUsuario = async () => {
-      try {
-        const resposta = await fetch("http://localhost:3000/usuarios");
-        const dados = await resposta.json();
-        setUsuarios(dados);
-      } catch {
-        alert('Ocorreu um erro no app!');
+      if(ListaCarros.length === 0){
+        return(
+            <h1>Lista vazia</h1>
+        )
       }
-    }
-    buscarUsuario();
-  }, [usuarios])
 
-  const remover = async(id) => {
-    try{
-      await fetch('http://localhost:3000/usuarios/'+id,{
-        method: 'delete'
-      });      
-    }catch{
-      alert('ixi lascou')
-    }
-  };
-
-      const exportPDF = () => {
-        const doc = new jsPDF ();
-        const tabelaDados = usuarios.map((usuario) => [
-          usuario.nome,
-          usuario.email,
-        ]);
-
-        doc.text("Lista de Usuário", 10, 10);
-        doc.autoTable({
-          head: [["nome", "E-mail"]],
-          body: tabelaDados,
-        });
-        
-        doc.save("alunosIFMS.pdf");
+      const RemoverCarro = async (id) => {
+        try {
+          const resposta = await fetch(`http://localhost:3000/carros/${id}`, {
+            method: "DELETE",
+          });
+          if (!resposta.ok) {
+            throw new Error('Erro ao remover o carro');
+          }
+          const novaLista = ListaCarros.filter((carro) => carro.id !== id);
+          setListaCarros(novaLista);
+        } catch (error) {
+          console.error("Erro ao remover o carro:", error);
+        }
       };
 
-  return (
-    <table>
-      <tr>
-        <td>Nome</td>
-        <td>E-mail</td>
-      </tr>
-      {usuarios.map((usuario) =>
-        <tr key={usuario.id}>
-          <td>{usuario.nome}</td>
-          <td>{usuario.email}</td>
-          <td><button onClick={() => remover(usuario.id)}/>remover</td>
-        </tr>
-      )}
-    </table>
-  );
+    return (
+        <>
+            <div>
+                <Carousel infiniteLoop useKeyboardArrows autoPlay showArrows={true} showStatus={false} showThumbs={false} dynamicHeight>
+                    <div>
+                        <img src="https://wyantgroup.com/wp-content/uploads/2017/02/porsche-banner-3.jpg" />
+                    </div>
+                    <div>
+                        <img src="https://www.porsche-moscow.ru/files/43765/normal.jpg" />
+                    </div>
+                    <div>
+                        <img src="https://gld-creative.s3.us-west-2.amazonaws.com/2024-porsche-911-gt3---banner-74b5c565b28f-1920x600.png" />
+                    </div>
+                </Carousel>
+            </div>
+            <div className={homeStyles.centerListaProdutos}>
+                <h1 className={homeStyles.titleProdutos}>HOME</h1>
+                <a href="/registro">
+                <button>Registrar Carro</button></a>
+                    <div className={ListaCarros.displayProdutos}>
+                    <ListaProdutos produtos={ListaCarros} removerCarro={RemoverCarro}/>
+                </div>
+            </div>
+        </>
+    );
 }
--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,38 +1,48 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import styles from '../styles/Register.module.css';
+import { Button } from '@mui/material';
 
 export default function Registrar() {
- const [nome, setNome] = useState('');
- const [email, setEmail] = useState('');
+  const [nomeCarro, setNomeCarro] = useState('');
+  const [marca, setMarca] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [cor, setCor] = useState('');
+  const [preco, setPreco] = useState('');
 
-   const navigate = useNavigate();
-
-  const registrar = async(event) =>{
-event.preverDefault();
- try{
-  const resposta = await fetch("http://localhost:3000/usuarios",{
-    method: 'POST',
-    headers: {'constent-type': 'Application/json'},
-    body: JSON.stringify({
-      nome: nome,
-      email: email
-    })
-  });
- if (resposta.ok){
-  navigate('/');
- } 
-}catch(err){
-  alert('erro no registro!', err);
-}
-  }
+  const Registrar = async (event) => {
+    event.preventDefault();
+    console.log('Registrando Carro...');
+    try {
+      await fetch('http://localhost:3000/carros', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          nomeCarro: nomeCarro,
+          marca: marca,
+          imagem: imagem,
+          cor: cor,
+          preco: preco
+        })
+      });
+      console.log('Carro registrado com sucesso!');
+      alert("Carro registrado com sucesso!");
+    } catch (error) {
+      alert("Ocorreu um erro na API");
+      console.error(error);
+    }
+  };
 
   return (
-        <main>
-          <form action="" onSubmit={registrar}>
-            <input type="text" value={nome} onChange={(event)=> setNome(event.target.value)}/>
-            <input type="email" value={email} onChange={(event)=> setEmail(event.target.value)}/>
-            <button>Salvar</button>
-          </form>
-        </main>
+    <main className={styles.main}>
+      <h1>Registro de Carros:</h1>
+      <form onSubmit={Registrar}>
+        <input placeholder="Insira o nome do carro" value={nomeCarro} onChange={(event) => setNomeCarro(event.target.value)} />
+        <input placeholder="Insira a marca do carro" value={marca} onChange={(event) => setMarca(event.target.value)} />
+        <input placeholder="Insira o link da imagem" value={imagem} onChange={(event) => setImagem(event.target.value)} />
+        <input placeholder="Insira a cor do carro" value={cor} onChange={(event) => setCor(event.target.value)} />
+        <input placeholder="Insira o preço do carro" type="number" value={preco} onChange={(event) => setPreco(event.target.value)} />
+        <Button variant="contained" color="success" type="submit">Registrar</Button>
+      </form>
+    </main>
   );
 }
